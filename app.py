980f80cb5ff496d14c00b49220765b9dfe5a4ada
Thanadonn <89176736+Thanadonn@users.noreply.github.com>
2022-01-09 19:46:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,13 +149,13 @@ def event_handle(event,json_line):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        if msg == "สวัสดี":
+        if msg == ("สวัสดี") :
             replyObj = TextSendMessage(text="จ้า ดีด้วยจ้า")
             line_bot_api.reply_message(rtoken, replyObj)
-        elif msg == "กินข้าวไหม":
+        elif msg == ("กินข้าวไหม") :
             replyObj = TextSendMessage(text="ไม่ล่ะ ขอเป็นสเต็ก")  
             line_bot_api.reply_message(rtoken, replyObj)
-        elif msg == "ไปเที่ยวกันไหม":    
+        elif msg == ("ไปเที่ยวกันไหม") :    
             replyObj = TextSendMessage(text="ไปดิ")  
             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "covid" :
